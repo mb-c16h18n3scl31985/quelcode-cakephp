9fb8cdc9f4542552b302ed2b8817c62dd6d8641e
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-01-09 09:09:55 +0900

    Fix: AuctionController.php/add.ctpへの値の送り方修正

--- html/mycakeapp/src/Controller/AuctionController.php
+++ html/mycakeapp/src/Controller/AuctionController.php
@@ -130,6 +130,7 @@ class AuctionController extends AuctionBaseController
     {
         //Biditemインスタンスを用意
         $biditem = $this->Biditems->newEntity();
+        var_dump($biditem);
 
         //イメージファイルをcakePHP側に保存
         if ($this->request->is('post')) {
@@ -180,9 +181,8 @@ class AuctionController extends AuctionBaseController
                 $connection->rollback();
             }
             //-------------------------トランザクションここまで-------------------------
-
-            $this->set(compact('biditem'));
         }
+        $this->set(compact('biditem'));
     }
 
 
